d716ef2db12474749fe5594add13d929e708d1d4
Ikram <=>
2023-06-30 18:58:45 +0400

    infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { QueryFunction, useQueries } from "react-query";
+import { useEffect, useRef } from "react";
+import { QueryFunction, useInfiniteQuery, useQuery } from "react-query";
 
 interface Post {
   body: string;
@@ -14,11 +15,11 @@ interface Todo {
   completed: boolean;
 }
 
-const getPosts: QueryFunction<Post[]> = async ({ signal }) => {
+const getPosts: QueryFunction<Post[]> = async (props) => {
   const data: Post[] = await fetch(
     "https://jsonplaceholder.typicode.com/posts",
     {
-      signal,
+      signal: props.signal,
     }
   ).then((res) => res.json());
   // SHUFFLE DATa
@@ -39,7 +40,7 @@ const getTodos: QueryFunction<Todo[]> = async ({ signal }) => {
   ).then((res) => res.json());
   // SHUFFLE DATa
   const arr: Todo[] = [];
-  while (data.length > 0) {
+  while (data.length > 100) {
     const random = Math.floor(Math.random() * data.length);
     arr.push(data.splice(random, 1)[0]);
   }
@@ -47,26 +48,44 @@ const getTodos: QueryFunction<Todo[]> = async ({ signal }) => {
 };
 
 function App() {
-  const [postsData, todosData] = useQueries([
-    {
+  const { data, refetch, isLoading, isFetchingNextPage, fetchNextPage } =
+    useInfiniteQuery<Post[]>({
       queryKey: ["posts"],
       queryFn: getPosts,
-    },
-    {
-      queryKey: ["todos"],
-      queryFn: getTodos,
-    },
-  ]);
+      getNextPageParam: (lastPage) => lastPage.length + 20,
+    });
 
-  console.log(postsData, todosData);
+  const todosData = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
 
   function refetchData(type: "posts" | "todos") {
-    if (type === "posts") return postsData.refetch();
+    if (type === "posts") return fetchNextPage();
     return todosData.refetch();
   }
 
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { height: h } = container.current?.getBoundingClientRect() as DOMRect;
+    const handleScroll = async () => {
+      if (isFetchingNextPage) return;
+      const st = container.current?.scrollTop as number;
+      const sh = container.current?.scrollHeight as number;
+      const scroll = st + h;
+      const boundary = 200;
+      if (scroll > sh - boundary) refetchData("posts");
+    };
+    container.current?.addEventListener("scroll", handleScroll);
+    return () => container.current?.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="flex items-start gap-3">
+    <div
+      ref={container}
+      className="h-screen overflow-y-auto flex items-start gap-3"
+    >
       <div className="flex flex-col gap-4 flex-1 p-3">
         <div className="flex items-center justify-between px-2">
           <h1 className="text-gray-900 text-lg font-semibold">Posts</h1>
@@ -79,19 +98,31 @@ function App() {
           </button>
         </div>
         <hr className="bg-gray-500" />
-        {postsData.isLoading ? (
+        {isLoading ? (
           <p className="text-sm font-bold">Loading posts ...</p>
         ) : (
-          postsData.data?.map((post) => (
-            <div
-              key={`post_${post.id}`}
-              className="flex flex-col gap-2 border border-gray-400 p-3 rounded-md"
-            >
-              <p className="text-base font-bold text-gray-900">{post.title}</p>
-              <hr className="opacity-70" />
-              <p className="text-sm text-gray-700">{post.body}</p>
-            </div>
-          ))
+          <div className="flex flex-col gap-4">
+            {data?.pages.map((page, pi) =>
+              page.map((post, i) => (
+                <div
+                  key={`post_${post.id}`}
+                  className="flex flex-col gap-2 border border-gray-400 p-3 rounded-md"
+                >
+                  <p>{pi * 100 + i}</p>
+                  <p className="text-base font-bold text-gray-900">
+                    {post.title}
+                  </p>
+                  <hr className="opacity-70" />
+                  <p className="text-sm text-gray-700">{post.body}</p>
+                </div>
+              ))
+            )}
+            {isFetchingNextPage ? (
+              <div className="h-48 grid place-items-center">
+                Next page is been fetching
+              </div>
+            ) : null}
+          </div>
         )}
       </div>
       <div className="flex flex-col gap-4 flex-1 border-r border-gray-200 p-3">
